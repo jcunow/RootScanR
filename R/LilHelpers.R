dc0a96f157f21ab55281c6a89e2f0d2ecd868497
jcunow <johannes.cunow@gmail.com>
2024-07-14 13:44:30 +0200

    added input format

--- R/LilHelpers.R
+++ R/LilHelpers.R
@@ -2,15 +2,39 @@
 
 #' Skeletonize a segmented image
 #'
-#' @param img binary image, can be path or magick object
+#' @param img can be: path, array, raster, or magick object
 #' @param itr iterations of thinning. see magick::image_morphology
+#' @param kernel kernel used for image operation. Other operations than skeletonizing is possible.
 #'
 #' @return image in as magick object
 #' @export
+#' @details
+#' Uses magick::image_morphology but eases the image input format
+#'
 #'
 #' @examples img.skeleton = skeletonize("/path/image.png",itr = 2)
-skeletonize = function(img,itr = 2){
-  if(!is.character(img)){
+skeletonize = function(img,itr = 2, kernel = 'Skeleton:3'){
+# works but not with raster
+  # if(!is.character(img)){
+  #   img_array <- as.array(img)
+  #   if(!(max(img) > 1)){
+  #     img_array <- img_array * 255
+  #   }
+  #
+  #   # Convert to raw array (required by magick)
+  #   magick_img <- imager::cimg2magick(imager::as.cimg( img_array))
+  # }else{
+  #   magick_img = magick::image_read(img)
+  # }
+
+# works for raster, path, magick, array ?
+  if(is.character(img)){
+    magick_img = magick::image_read(img)
+  }else{
+    if(class(img) == "raster" |class(img) == "RasterBrick"){
+      magick_img = magick::image_read(raster::as.array(img))
+    }
+  }else{
     img_array <- as.array(img)
     if(!(max(img) > 1)){
       img_array <- img_array * 255
@@ -18,16 +42,12 @@ skeletonize = function(img,itr = 2){
 
     # Convert to raw array (required by magick)
     magick_img <- imager::cimg2magick(imager::as.cimg( img_array))
-  }else{
-    magick_img = magick::image_read(img)
   }
 
   grayscale_img = magick::image_channel(magick_img,"lightness")
+  # core fucntion, Perform morphological thinning
+  thinned_img <- image_morphology(grayscale_img, 'Thinning', kernel= kernel,iterations = itr)
 
-  # Perform morphological thinning
-  thinned_img <- image_morphology(grayscale_img, 'Thinning', 'Skeleton:3',iterations = itr)
-
-  print(thinned_img)
 
   return(thinned_img)
 }
@@ -69,39 +89,39 @@ rgb2gray = function(img, r=0.21,g=0.72,b=0.07){
 
 
 
-#' Resize the Image
+#' #' Resize the Image
+#' #'
+#' #' useful if some scanning campaigns have used different dpi
+#' #' @param import.path Input Path
+#' #' @param output.path Output Path
+#' #' @param height target height
+#' #' @param width target width
+#' #'
+#' #' @return a image with new dimensions and resolution
+#' #' @export
+#' #'
+#' #' @examples resize.image(import.path, output.path) = resized.image
+#' resize.image = function(import.path,output.path,height = 2550,width= 2273){
+#'   img.list = list.files(import.path, pattern = ".tiff")
 #'
-#' useful if some scanning campaigns have used different dpi
-#' @param import.path Input Path
-#' @param output.path Output Path
-#' @param height target height
-#' @param width target width
+#'   # sequentially read images, lower resolution and restore them
+#'   t1 = Sys.time()
+#'   for (i in 1:length(img.list)) {
+#'     # read
+#'     temp.im = OpenImageR::readImage(paste0(import.path,img.list[i]))
+#'     # resample bilinear
+#'     temp.im = OpenImageR::resizeImage(temp.im,
+#'                                       method = "bilinear",
+#'                                       height = 2550, width = 2273)
+#'     # transformation to 0-1 range
+#'     temp.im = imagefx::range01(temp.im)
+#'     # write as tiff
+#'     OpenImageR::writeImage(temp.im,file_name = paste0(output.path,img.list[i]))
 #'
-#' @return a image with new dimensions and resolution
-#' @export
+#'   }
+#'   t2 = Sys.time();floor(t2-t1)
 #'
-#' @examples resize.image(import.path, output.path) = resized.image
-resize.image = function(import.path,output.path,height = 2550,width= 2273){
-  img.list = list.files(import.path, pattern = ".tiff")
-
-  # sequentially read images, lower resolution and restore them
-  t1 = Sys.time()
-  for (i in 1:length(img.list)) {
-    # read
-    temp.im = OpenImageR::readImage(paste0(import.path,img.list[i]))
-    # resample bilinear
-    temp.im = OpenImageR::resizeImage(temp.im,
-                                      method = "bilinear",
-                                      height = 2550, width = 2273)
-    # transformation to 0-1 range
-    temp.im = imagefx::range01(temp.im)
-    # write as tiff
-    OpenImageR::writeImage(temp.im,file_name = paste0(output.path,img.list[i]))
-
-  }
-  t2 = Sys.time();floor(t2-t1)
-
-}
+#' }
 
 
 
