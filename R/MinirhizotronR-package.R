8e5a4807e454fce617f4f4b8a4ad8e9d8b54f0c8
jcunow <johannes.cunow@gmail.com>
2024-08-03 21:53:09 +0200

    new data

---
+++ R/MinirhizotronR-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom grDevices rgb2hsv
+#' @importFrom magrittr %>%
+#' @importFrom methods is
+#' @importFrom stats median
+#' @importFrom stats pchisq
+#' @importFrom stats pf
+#' @importFrom stats quantile
+#' @importFrom utils object.size
+## usethis namespace: end
+NULL
