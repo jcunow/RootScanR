7e1b52cf8408ab056874267240e26bb1561d0330
jcunow <johannes.cunow@gmail.com>
2024-09-10 23:11:18 +0200

    added wasserstein &  bugfix

--- R/compareDistributions.R
+++ R/compareDistributions.R
@@ -18,47 +18,79 @@
 
 
 
-# Define the tail weight function (e.g., exponential weight for tail emphasis)
-# lambda = shape parameter (0 = constant weighting), x0 = curve offset (= inflexion point )
-tail_weight_function <- function(x, parameter = list(lambda = 0.2,x0=30), method = "sigmoid",baseline.weight = 0.1) {
+#' Tail weight function (e.g., exponential weight for tail emphasis)
+#'
+#' @param Q probability vector 1
+#' @param P probability vector 2
+#' @param inverse changes from right tail to left tail if TRUE
+#' @param parameter list with lambda -> shape parameter (0 = constant weighting) & x0 -> curve offset (= inflexion point )
+#' @param method weighting function along index. Available options are: c("constant", "linear, "exponential", "sigmoid", "gompertz")
+#' @param baseline.weight  minimal weight between 0-1
+#' @keywords internal
+#' @export
+#' @return weights between 0-1 along the index
+#'
+#'
+#'
+#'
+#'
+tail_weight_function <- function(x, parameter = list(lambda = 0.2,x0=5),
+                                 method = "sigmoid",baseline.weight = 0,inverse =FALSE) {
 
+  x =x / sum(x,na.rm=TRUE)
   ## equal weighting along index
   if(method == "constant"){
-    method = "exp"
-    lambda = 0
+    method = "exponential"
+    parameter$lambda = 0
   }
   # exponential weighting function
-  if(method == "exp"){
+  if(method == "exponential"){
     # sample size
-    n = max(x,na.rm=TRUE)
+    n = length(x)
     # Normalize lambda based to make it index scale invariant
     lambda_adjusted <- parameter$lambda / n
     # Tail weight function based on the index of the probability
-    val = exp(-lambda_adjusted * abs(x - n))*(1-baseline.weight) + baseline.weight
+    weights = exp(-lambda_adjusted * abs(x - n))*(1-baseline.weight) + baseline.weight
   }
   # sigmoid weighting function
   if(method == "sigmoid"){
     # k = steepness of curve, x0 = inflection point
-   val =  1 / (1 + exp(-parameter$lambda * (x - parameter$x0))) *(1-baseline.weight) + baseline.weight
+    weights =  1 / (1 + exp(-parameter$lambda * (x - parameter$x0))) *(1-baseline.weight) + baseline.weight
   }
   # linerarily increasing weights
   if(method == "linear"){
     # Normalize linear weighting based on the sample size
-    val = (x / length(x))*(1-baseline.weight) + baseline.weight
+    weights = (x / length(x))*(1-baseline.weight) + baseline.weight
   }
   # gompertz weighting
   if(method == "gompertz"){
-      val = (1/1+baseline.weight) * exp(-parameter$x0 * exp(-parameter$lambda * x)) *(1-baseline.weight) + baseline.weight
+    weights = (1/1+baseline.weight) * exp(-parameter$x0 * exp(-parameter$lambda * x)) *(1-baseline.weight) + baseline.weight
+  }
+  if(inverse == TRUE){
+    weights = rev(weights)
+
   }
 
-  return(val)
+  weights = weights / sum(weights,na.rm=TRUE)
+  return(weights)
 }
 
 
 
 
-# Calculate tail-weighted KL divergence for discrete distributions
-tail_weighted_kl_divergence <- function(P, Q, tail_weight_function, parameter = list(lambda = 0.2,x0=30)) {
+#
+#' Calculate tail-weighted KL divergence for discrete distributions
+#'
+#' @param Q probability vector 1
+#' @param P probability vector 2
+#' @param inverse changes from right tail to left tail if TRUE
+#' @param parameter list with lambda -> shape parameter (0 = constant weighting) & x0 -> curve offset (= inflexion point )
+#' @param method weighting function along index. Available options are: c("constant", "linear, "exponential", "sigmoid", "gompertz")
+#'
+#' @return KL divergence, not symmetrical - changing the input order will chenge the result
+#' @export
+#'
+tail_weighted_kl_divergence <- function(P, Q, parameter = list(lambda = 0.2,x0=30),inverse=FALSE,method = "constant") {
   # Ensure that the distributions are the same length
   if (length(P) != length(Q)) {
     stop("Distributions P and Q must be of the same length.")
@@ -74,7 +106,7 @@ tail_weighted_kl_divergence <- function(P, Q, tail_weight_function, parameter = 
     if (p_x == 0 || q_x == 0) {
       return(0)
     }
-    weight <- tail_weight_function(i,  parameter)
+    weight <- tail_weight_function(i,  parameter,method = method, inverse=inverse)
     return(weight * p_x * log(p_x / q_x))
   }))
 
@@ -82,14 +114,36 @@ tail_weighted_kl_divergence <- function(P, Q, tail_weight_function, parameter = 
 }
 
 
-# Compute the tail-weighted Jensen-Shannon divergence
-tail_weighted_js_divergence <- function(P, Q, tail_weight_function, parameter = list(lambda = 0.2,x0=30)) {
+
+
+
+#' Compute the tail-weighted Jensen-Shannon divergence
+#'
+#' @param Q probability vector 1
+#' @param P probability vector 2
+#' @param inverse changes from right tail to left tail if TRUE
+#' @param parameter list with lambda -> shape parameter (0 = constant weighting) & x0 -> curve offset (= inflexion point )
+#' @param method weighting function along index. Available options are: c("constant", "linear, "exponential", "sigmoid", "gompertz")
+#'
+#' @return Jensen-Shannon Divergence - symmetric version of KL
+#' @export
+#'
+#' @examples
+#' P <- c(0.025,0.05,0.1,0.15, 0.2, 0.3,0.4, 0.5,0.3,0.1)  # Distribution P
+#' Q <- c(0.025,0.05,0.1,0.15, 0.2, 0.3,0.4, 0.5,0.3,0.1)**6  # Distribution Q
+#'
+#' # Ensure the distributions are valid (non-negative and sum to 1)
+#' P <- P / sum(P)
+#' Q <- Q / sum(Q)
+#'
+#' tail_weighted_js_divergence(P,Q,tail_weight_function(P,method="sigmoid),parameter = list(lambda = 0.2,x0=30))
+tail_weighted_js_divergence <- function(P, Q,  parameter = list(lambda = 0.2,x0=30),method="constant",inverse=FALSE) {
   # Compute the average distribution
   M <- (P + Q) / 2
 
   # Compute the tail-weighted KL divergences
-  KL_PM <- tail_weighted_kl_divergence(P, M, tail_weight_function, parameter)
-  KL_QM <- tail_weighted_kl_divergence(Q, M, tail_weight_function, parameter)
+  KL_PM <- tail_weighted_kl_divergence(P, M, tail_weight_function(inverse = inverse,method=method), parameter)
+  KL_QM <- tail_weighted_kl_divergence(Q, M, tail_weight_function(inverse = inverse,method=method), parameter)
 
   # Average the KL divergences
   js_divergence <- (KL_PM + KL_QM) / 2
@@ -100,7 +154,38 @@ tail_weighted_js_divergence <- function(P, Q, tail_weight_function, parameter = 
 
 
 # Compute the tail-weighted Jensen-Shannon divergence
-parameter = list(lambda = 0.2,x0=30)  # Tail weight parameter
-js_divergence <- tail_weighted_js_divergence(P, Q, tail_weight_function, parameter)
-print(paste("Tail-Weighted Jensen-Shannon Divergence:", js_divergence))
+#parameter = list(lambda = 0.2,x0=30)  # Tail weight parameter
+#js_divergence <- tail_weighted_js_divergence(P, Q, tail_weight_function, parameter)
+#print(paste("Tail-Weighted Jensen-Shannon Divergence:", js_divergence))
 
+
+
+
+
+#' A tailweighted Version of 1 dimensional Wasserstein distance betwwen two probability vectors
+#'
+#' @param Q probability vector 1
+#' @param P probability vector 2
+#' @param inverse changes from right tail to left tail if TRUE
+#' @param parameter list with lambda -> shape parameter (0 = constant weighting) & x0 -> curve offset (= inflexion point )
+#' @param method weighting function along index. Available options are: c("constant", "linear, "exponential", "sigmoid", "gompertz")
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' P <- c(0.025,0.05,0.1,0.15, 0.2, 0.3,0.4, 0.5,0.3,0.1)  # Distribution P
+#' Q <- c(0.025,0.05,0.1,0.15, 0.2, 0.3,0.4, 0.5,0.3,0.1)**6  # Distribution Q
+#'
+#' # Ensure the distributions are valid (non-negative and sum to 1)
+#' P <- P / sum(P)
+#' Q <- Q / sum(Q)
+#'
+#' tail_weighted_wasserstein_distance(P,Q,inverse=F,method="constant",parameter = list(lambda = 0.2,x0=3))
+tail_weighted_wasserstein_distance = function(Q,P,inverse=F,parameter = list(lambda = 0.2,x0=3),method= "constant"){
+  # tail weighting
+  weighted.P = tail_weight_function(P,inverse=inverse,parameter = parameter,method = method )
+  weighted.Q = tail_weight_function(Q,inverse=inverse,parameter = parameter,method = method)
+  # distance computation
+  transport::wasserstein1d(a=weighted.P,b=weighted.Q)
+}
